9b77c872be2eb6dde8fe70e1504747559ae84cf4
André-Marthin Helberg <helberg.andre@gmail.com>
2024-03-01 10:42:06 +0200

    Upgrade hyper to 1.2.0 (#8)
    * WIP
    
    * Cleanup warnings
    
    * Proxy path updated to take request
    
    This ensures there's only 1 place that req->fs path logic exists
    
    * Remove nonsensical comments
    
    * Fix formatting

---
+++ src/clone.rs
@@ -0,0 +1,73 @@
+use crate::http_utils::full;
+use bytes::Bytes;
+use http_body_util::combinators::BoxBody;
+use http_body_util::BodyExt;
+use hyper;
+use hyper::body::{Buf, Incoming};
+use hyper::{Request, Response};
+
+macro_rules! two_of {
+    // The macro will match against any type `T`
+    ($type:ty) => {
+        // And it will produce a tuple of two of that type
+        ($type, $type)
+    };
+}
+pub fn clone_body<T: Buf>(mut y: T) -> Vec<u8> {
+    let mut body: Vec<u8> = Vec::with_capacity(1000);
+    while y.has_remaining() {
+        if body.capacity() == body.len() {
+            body.reserve(100);
+        }
+        body.push(y.get_u8());
+    }
+    body
+}
+
+type MyResponse = Result<two_of!(Response<BoxBody<Bytes, hyper::Error>>), hyper::Error>;
+type MyRequest = Result<two_of!(Request<BoxBody<Bytes, hyper::Error>>), hyper::Error>;
+
+pub async fn clone_incoming_request(req: Request<Incoming>) -> MyRequest {
+    let (parts, body) = req.into_parts();
+    let x = body.collect().await?.aggregate();
+    let body = clone_body(x);
+
+    Ok((
+        Request::from_parts(parts.clone(), full(body.clone())),
+        Request::from_parts(parts.clone(), full(body.clone())),
+    ))
+}
+
+#[allow(dead_code)]
+pub async fn clone_bytes_request(req: Request<BoxBody<Bytes, hyper::Error>>) -> MyRequest {
+    let (parts, body) = req.into_parts();
+    let x = body.collect().await?.aggregate();
+    let body = clone_body(x);
+
+    Ok((
+        Request::from_parts(parts.clone(), full(body.clone())),
+        Request::from_parts(parts, full(body)),
+    ))
+}
+// Response
+pub async fn clone_incoming_response(req: Response<Incoming>) -> MyResponse {
+    let (parts, body) = req.into_parts();
+    let x = body.collect().await?.aggregate();
+    let body = clone_body(x);
+
+    Ok((
+        Response::from_parts(parts.clone(), full(body.clone())),
+        Response::from_parts(parts.clone(), full(body.clone())),
+    ))
+}
+
+pub async fn clone_bytes_response(req: Response<BoxBody<Bytes, hyper::Error>>) -> MyResponse {
+    let (parts, body) = req.into_parts();
+    let x = body.collect().await?.aggregate();
+    let body = clone_body(x);
+
+    Ok((
+        Response::from_parts(parts.clone(), full(body.clone())),
+        Response::from_parts(parts, full(body)),
+    ))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,147 +1,103 @@
+mod clone;
 mod config;
+mod http_utils;
+mod proxy;
+mod tokiort;
 
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Client;
-use hyper::{Body, Request, Response, Server};
-use hyper_rustls::ConfigBuilderExt;
+use hyper::service::service_fn;
 
-use std::convert::Infallible;
+use http_body_util::combinators::BoxBody;
+use hyper::{server, Request, Response};
+
+use bytes::Bytes;
+use tokio::net::{TcpListener, TcpStream};
+
+use crate::tokiort::TokioIo;
 use std::net::{IpAddr, SocketAddr};
-use std::path::Path;
 use std::str::FromStr;
-use tokio::fs;
-use tokio::io::AsyncWriteExt;
 
-fn recording_exists(recording_name: &str) -> bool {
-    Path::new(&recording_name).exists()
+use hyper::upgrade::Upgraded;
+use hyper::Method;
+
+fn host_addr(uri: &http::Uri) -> Option<String> {
+    uri.authority().and_then(|auth| Some(auth.to_string()))
 }
 
-fn recording_name(folder: &str, path: &str, method: &str) -> String {
-    format!("{}/{}/{}", folder, path, method)
+// Create a TCP connection to host:port, build a tunnel between the connection and
+// the upgraded connection
+async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
+    // Connect to remote server
+    let mut server = TcpStream::connect(addr).await?;
+    let mut upgraded = TokioIo::new(upgraded);
+
+    // Proxying data
+    let (_from_client, _from_server) =
+        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    Ok(())
 }
 
 async fn proxy_handler(
-    config: config::Config,
-    req: Request<Body>,
-) -> Result<Response<Body>, hyper::Error> {
-    // Prepare the TLS client config
-    let tls = rustls::ClientConfig::builder()
-        .with_safe_defaults()
-        .with_native_roots()
-        .with_no_client_auth();
-    // Prepare the HTTPS connector
-    let https = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_tls_config(tls)
-        .https_or_http()
-        .enable_http1()
-        .build();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    let upstream = config.upstream;
+    config: &config::Config,
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     let method = req.method().clone();
-    let path = req.uri().clone();
-
-    let uri = format!("{}{}", upstream, &path);
-
+    let path = req.uri().path();
     println!("request  for     {} {}", &method, &path);
 
-    let recording_path = recording_name(&config.tapes, &req.uri().to_string(), &method.to_string());
-    fs::create_dir_all(format!("{}/{}", &config.tapes, &req.uri().to_string()))
-        .await
-        .expect("Failed to create a tape directory");
-
-    if recording_exists(&recording_path) {
-        //let contents = fs::read_to_string(&recording_path).await.unwrap();
-        let c: Vec<u8> = fs::read(&recording_path).await.unwrap();
-        let mut headers = [httparse::EMPTY_HEADER; 64];
-        let mut resp = httparse::Response::new(&mut headers);
-
-        resp.parse(&c).unwrap();
-
-        let mut start_of_body = 0;
-
-        for i in 0..c.len() {
-            let bs = "\r\n".as_bytes();
-            if i + 3 < c.len() {
-                if c[i] == bs[0] && c[i + 1] == bs[1] && c[i + 2] == bs[0] && c[i + 3] == bs[1] {
-                    start_of_body = i + 4;
-                    break;
+    if Method::CONNECT == req.method() {
+        // Received an HTTP request like:
+        // ```
+        // CONNECT www.domain.com:443 HTTP/1.1
+        // Host: www.domain.com:443
+        // Proxy-Connection: Keep-Alive
+        // ```
+        //
+        // When HTTP method is CONNECT we should return an empty body
+        // then we can eventually upgrade the connection and talk a new protocol.
+        //
+        // Note: only after client received an empty body with STATUS_OK can the
+        // connection be upgraded, so we can't return a response inside
+        // `on_upgrade` future.
+        if let Some(addr) = host_addr(req.uri()) {
+            tokio::task::spawn(async move {
+                match hyper::upgrade::on(req).await {
+                    Ok(upgraded) => {
+                        if let Err(e) = tunnel(upgraded, addr).await {
+                            eprintln!("server io error: {}", e);
+                        };
+                    }
+                    Err(e) => eprintln!("upgrade error: {}", e),
                 }
-            }
-        }
+            });
 
-        let mut res = Response::builder().status(resp.code.unwrap());
-        for i in resp.headers {
-            res = res.header(i.name, i.value);
+            Ok(Response::new(http_utils::empty()))
+        } else {
+            eprintln!("CONNECT host is not socket addr: {:?}", req.uri());
+            let mut resp = Response::new(http_utils::full("CONNECT must be to a socket address"));
+            *resp.status_mut() = http::StatusCode::BAD_REQUEST;
+
+            Ok(resp)
         }
-        let bs: Vec<u8> = c[start_of_body..].try_into().unwrap();
-        let res = res.body(Body::from(bs)).unwrap();
-        println!("playback for {} {} {}", resp.code.unwrap(), &method, &path);
-        Ok(res)
     } else {
         if config.replay_only {
-            println!("Not Impl for {} {} {}", 501, &method, &path);
-            let mut resp = Response::builder().status(501);
-
-            if req.headers().get("accept").is_some() {
-                resp = resp.header("accept", req.headers().get("accept").unwrap());
+            proxy::replay(config, req).await
+        } else {
+            if proxy::recording_exists(&proxy::recording_name(&config.tapes, &req)) {
+                return proxy::replay(config, req).await;
             }
-            return Ok(resp.body(Body::from("")).unwrap());
+
+            let (req, new_req) = clone::clone_incoming_request(req).await?;
+            let resp = proxy::make_request(new_req).await?;
+            let (resp, new_resp) = clone::clone_incoming_response(resp).await?;
+            let _ = proxy::record(&config, req, new_resp).await;
+            Ok(resp)
         }
-        let mut outgoing_request = Request::builder()
-            .method(method.clone())
-            .uri(uri)
-            .version(req.version());
-
-        for (k, v) in req.headers() {
-            outgoing_request = outgoing_request.header(k, v);
-        }
-
-        let outgoing_request = outgoing_request.body(req.into_body()).unwrap();
-
-        let client_response = client.request(outgoing_request).await?;
-
-        // Modify the response as needed
-        let modified_response = client_response;
-
-        let mut file = tokio::fs::File::create(&recording_path)
-            .await
-            .expect("Could not write to the tapes directory");
-
-        let header = format!(
-            "{:?} {} {}\r\n",
-            &modified_response.version(),
-            &modified_response.status().as_str(),
-            &modified_response
-                .status()
-                .canonical_reason()
-                .or(Some(""))
-                .unwrap()
-        );
-        let _ = file.write_all(header.as_bytes()).await;
-        for (name, value) in modified_response.headers() {
-            let header = format!("{}:{}\r\n", name, value.to_str().unwrap());
-            let _ = file.write_all(header.as_bytes()).await;
-        }
-        let _ = file.write_all("\r\n".as_bytes()).await;
-
-        let (parts, body) = modified_response.into_parts();
-        let bytes = hyper::body::to_bytes(body).await.unwrap();
-
-        let _ = file.write_all(&bytes.clone()).await;
-
-        println!(
-            "record   for {} {} {}",
-            &parts.status.as_u16(),
-            &method,
-            &path
-        );
-        Ok(Response::from_parts(parts, Body::from(bytes)))
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_config().await;
 
     let ip =
@@ -149,15 +105,22 @@ async fn main() {
     let addr = SocketAddr::new(ip, config.port);
     println!("Listening on {}", addr);
 
-    let make_service = make_service_fn(move |_conn| {
+    let listener = TcpListener::bind(&addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
         let config = config.clone();
-        async move { Ok::<_, Infallible>(service_fn(move |req| proxy_handler(config.clone(), req))) }
-    });
-
-    let server = Server::bind(&addr).serve(make_service);
-
-    // And run forever...
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+        tokio::task::spawn(async move {
+            if let Err(err) = server::conn::http1::Builder::new()
+                .serve_connection(
+                    io,
+                    service_fn(|req| async { proxy_handler(&config, req).await }),
+                )
+                .with_upgrades()
+                .await
+            {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
     }
 }
